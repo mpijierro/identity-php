9268d2ebc887aeb8085213c4052a0c7a8e5ac21a
Manu Pijierro <manuel.pijierro@sngular.team>
2019-04-10 12:00:10 +0200

    add source code and test

---
+++ src/Identity.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace MPijierro\IdentityPhp;
+
+class Identity
+{
+    public function isValidIban($aIbanNumber)
+    {
+
+        $iban = new \IBAN();
+
+        return $iban->Verify($aIbanNumber);
+
+    }
+
+    public function documentIsValid($documentId)
+    {
+
+        return ($this->isValidCif($documentId) OR $this->isValidNif($documentId) OR ($this->isValidNie($documentId)));
+
+    }
+
+    public function isValidCif($cif)
+    {
+
+        $cifRegEx1 = '/^[ABEH][0-9]{8}$/i';
+        $cifRegEx2 = '/^[KPQS][0-9]{7}[A-J]$/i';
+        $cifRegEx3 = '/^[CDFGJLMNRUVW][0-9]{7}[0-9A-J]$/i';
+
+        if (preg_match($cifRegEx1, $cif) || preg_match($cifRegEx2, $cif) || preg_match($cifRegEx3, $cif)) {
+            $control = $cif[strlen($cif) - 1];
+            $suma_A = 0;
+            $suma_B = 0;
+
+            for ($i = 1; $i < 8; $i++) {
+                if ($i % 2 == 0) {
+                    $suma_A += intval($cif[$i]);
+                } else {
+                    $t = (intval($cif[$i]) * 2);
+                    $p = 0;
+
+                    for ($j = 0; $j < strlen($t); $j++) {
+                        $p += substr($t, $j, 1);
+                    }
+                    $suma_B += $p;
+                }
+            }
+
+            $suma_C = (intval($suma_A + $suma_B))."";
+            $suma_D = (10 - intval($suma_C[strlen($suma_C) - 1])) % 10;
+
+            $letras = "JABCDEFGHI";
+
+            if ($control >= "0" && $control <= "9") {
+                return ($control == $suma_D);
+            }
+
+            return (strtoupper($control) == $letras[$suma_D]);
+        }
+
+        return false;
+    }
+
+    public function isValidNif($nif)
+    {
+        $nifRegEx = '/^[0-9]{8}[A-Z]$/i';
+
+        $letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+        if (preg_match($nifRegEx, $nif)) {
+            return ($letras[(substr($nif, 0, 8) % 23)] == $nif[8]);
+        }
+
+        return false;
+    }
+
+    public function isValidNie($nif)
+    {
+        $nieRegEx = '/^[KLMXYZ][0-9]{7}[A-Z]$/i';
+        $letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+        if (preg_match($nieRegEx, $nif)) {
+
+            $r = str_replace(['X', 'Y', 'Z'], [0, 1, 2], $nif);
+
+            return ($letras[(substr($r, 0, 8) % 23)] == $nif[8]);
+        }
+
+        return false;
+
+    }
+
+    public function sanitize($documentId)
+    {
+        $sanitizeDocumentId = trim($documentId);
+        $sanitizeDocumentId = strtoupper($sanitizeDocumentId);
+
+        return preg_replace("/[^A-Za-z0-9]/", "", $sanitizeDocumentId);
+    }
+}
---
+++ tests/IdentityTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace MPijierro\IdentityPhp\tests;
+
+use MPijierro\IdentityPhp\Identity;
+use PHPUnit\Framework\TestCase;
+
+class IdentityTest extends TestCase
+{
+
+    /**
+     * @var Identity
+     */
+    private $identity;
+
+
+    public function setUp()
+    {
+        $this->identity = new Identity();
+    }
+
+
+    public function test_sanitize()
+    {
+        $this->assertEquals('Q6887124C', $this->identity->sanitize('q6887124c'));
+        $this->assertEquals('Q6887124C', $this->identity->sanitize('q6887124C    '));
+        $this->assertEquals('000Q6887124C', $this->identity->sanitize('000q6887124C    '));
+    }
+
+    public function test_valid_cif()
+    {
+        $this->assertTrue($this->identity->isValidCif('Q6887124C'));
+        $this->assertTrue($this->identity->isValidCif('J51062271'));
+        $this->assertTrue($this->identity->isValidCif('D9990690A'));
+        $this->assertTrue($this->identity->isValidCif('N8796829C'));
+    }
+
+    public function test_invalid_cif()
+    {
+        $this->assertFalse($this->identity->isValidCif('Q6887124'));
+        $this->assertFalse($this->identity->isValidCif('Q6887123C'));
+        $this->assertFalse($this->identity->isValidCif('6887123C'));
+        $this->assertFalse($this->identity->isValidCif(''));
+        $this->assertFalse($this->identity->isValidCif('AAAAAAAAAAAAAAA'));
+        $this->assertFalse($this->identity->isValidCif('Q6887|124C'));
+        $this->assertFalse($this->identity->isValidCif(null));
+    }
+
+    public function test_valid_nif()
+    {
+        $this->assertTrue($this->identity->isValidNif('21361012S'));
+        $this->assertTrue($this->identity->isValidNif('64160547T'));
+        $this->assertTrue($this->identity->isValidNif('48692083W'));
+        $this->assertTrue($this->identity->isValidNif('08861617Q'));
+    }
+
+    public function test_invalid_nif()
+    {
+        $this->assertFalse($this->identity->isValidNif('21361012'));
+        $this->assertFalse($this->identity->isValidNif('1361012S'));
+        $this->assertFalse($this->identity->isValidNif('12345678F'));
+        $this->assertFalse($this->identity->isValidNif('008861617Q'));
+        $this->assertFalse($this->identity->isValidNif(''));
+        $this->assertFalse($this->identity->isValidNif(null));
+
+        $this->assertTrue($this->identity->isValidNif('08861617Q'));
+        $this->assertFalse($this->identity->isValidNif('08861|617Q'));
+
+        $this->assertFalse($this->identity->isValidNif('X3212050P')); //nie
+    }
+
+
+    public function test_valid_nie()
+    {
+        $this->assertTrue($this->identity->isValidNie('X3212050P'));
+        $this->assertTrue($this->identity->isValidNie('X2792997S'));
+    }
+
+
+    public function test_invalid_nie()
+    {
+        $this->assertFalse($this->identity->isValidNie('Z8930474'));
+        $this->assertFalse($this->identity->isValidNie('8930474Q'));
+        $this->assertFalse($this->identity->isValidNie('Z893999Q'));
+        $this->assertFalse($this->identity->isValidNie(''));
+        $this->assertFalse($this->identity->isValidNie(null));
+
+        $this->assertTrue($this->identity->isValidNie('X3212050P'));
+        $this->assertFalse($this->identity->isValidNie('X321|2050P'));
+        $this->assertFalse($this->identity->isValidNie('08861617Q')); //nif
+    }
+
+
+    public function test_document_is_any_valid()
+    {
+
+        $this->assertTrue($this->identity->documentIsValid('21361012S'));
+        $this->assertTrue($this->identity->documentIsValid('21361012S'));
+        $this->assertTrue($this->identity->documentIsValid('X3212050P'));
+        $this->assertFalse($this->identity->documentIsValid('Q6887124'));
+        $this->assertFalse($this->identity->documentIsValid('21361012'));
+        $this->assertFalse($this->identity->documentIsValid('Z893999Q'));
+
+    }
+
+
+    public function test_valid_iban()
+    {
+        $this->assertTrue($this->identity->isValidIban('ES9100490013112991609374'));
+        $this->assertTrue($this->identity->isValidIban('ES09 2038-0626-0160-0002-5280'));
+        $this->assertTrue($this->identity->isValidIban('ES 58 0075 0204 9406 0081 1004'));
+        $this->assertTrue($this->identity->isValidIban('ES98 – 3190 – 0974 – 34 - 4255071823'));
+        $this->assertTrue($this->identity->isValidIban('ES31-2080-5155-9730-4000-0250'));
+        $this->assertTrue($this->identity->isValidIban('ES94 2095 5381 1910 6117 3539'));
+
+    }
+
+
+    public function test_invalid_iban()
+    {
+
+        $this->assertFalse($this->identity->isValidIban('ES9100490013112991609374a'));
+        $this->assertFalse($this->identity->isValidIban('9100490013112991609374a'));
+        $this->assertFalse($this->identity->isValidIban('9100490013112991609374a'));
+        $this->assertFalse($this->identity->isValidIban(''));
+        $this->assertFalse($this->identity->isValidIban(null));
+
+        $this->assertTrue($this->identity->isValidIban('ES94 2095 5381 1910 6117 3539'));
+        $this->assertFalse($this->identity->isValidIban('ES94 2095 53|1 1910 6117 3539'));
+
+    }
+
+}
