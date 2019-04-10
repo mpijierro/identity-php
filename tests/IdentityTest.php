d969353fd9bccd05cec0b0b76419bc8864e2d00c
Manu Pijierro <manuel.pijierro@sngular.team>
2019-04-10 12:57:57 +0200

    add test for testing exception

--- tests/IdentityTest.php
+++ tests/IdentityTest.php
@@ -7,127 +7,119 @@ use PHPUnit\Framework\TestCase;
 
 class IdentityTest extends TestCase
 {
-
     /**
      * @var Identity
      */
     private $identity;
-
 
     public function setUp()
     {
         $this->identity = new Identity();
     }
 
-
-    public function test_sanitize()
-    {
-        $this->assertEquals('Q6887124C', $this->identity->sanitize('q6887124c'));
-        $this->assertEquals('Q6887124C', $this->identity->sanitize('q6887124C    '));
-        $this->assertEquals('000Q6887124C', $this->identity->sanitize('000q6887124C    '));
-    }
-
     public function test_valid_cif()
     {
-        $this->assertTrue($this->identity->isValidCif('Q6887124C'));
-        $this->assertTrue($this->identity->isValidCif('J51062271'));
-        $this->assertTrue($this->identity->isValidCif('D9990690A'));
-        $this->assertTrue($this->identity->isValidCif('N8796829C'));
+        $this->assertTrue($this->identity->isValidCIF('Q6887124C'));
+        $this->assertTrue($this->identity->isValidCIF('J51062271'));
+        $this->assertTrue($this->identity->isValidCIF('D9990690A'));
+        $this->assertTrue($this->identity->isValidCIF('N8796829C'));
     }
 
     public function test_invalid_cif()
     {
-        $this->assertFalse($this->identity->isValidCif('Q6887124'));
-        $this->assertFalse($this->identity->isValidCif('Q6887123C'));
-        $this->assertFalse($this->identity->isValidCif('6887123C'));
-        $this->assertFalse($this->identity->isValidCif(''));
-        $this->assertFalse($this->identity->isValidCif('AAAAAAAAAAAAAAA'));
-        $this->assertFalse($this->identity->isValidCif('Q6887|124C'));
-        $this->assertFalse($this->identity->isValidCif(null));
+        $this->assertFalse($this->identity->isValidCIF('Q6887124'));
+        $this->assertFalse($this->identity->isValidCIF('Q6887123C'));
+        $this->assertFalse($this->identity->isValidCIF('6887123C'));
+        $this->assertFalse($this->identity->isValidCIF(''));
+        $this->assertFalse($this->identity->isValidCIF('AAAAAAAAAAAAAAA'));
+        $this->assertFalse($this->identity->isValidCIF('Q6887|124C'));
+    }
+
+    public function test_throw_exception_when_cif_not_is_string_value()
+    {
+        $this->expectException(\TypeError::class);
+        $this->assertFalse($this->identity->isValidCIF(null));
     }
 
     public function test_valid_nif()
     {
-        $this->assertTrue($this->identity->isValidNif('21361012S'));
-        $this->assertTrue($this->identity->isValidNif('64160547T'));
-        $this->assertTrue($this->identity->isValidNif('48692083W'));
-        $this->assertTrue($this->identity->isValidNif('08861617Q'));
+        $this->assertTrue($this->identity->isValidNIF('21361012S'));
+        $this->assertTrue($this->identity->isValidNIF('64160547T'));
+        $this->assertTrue($this->identity->isValidNIF('48692083W'));
+        $this->assertTrue($this->identity->isValidNIF('08861617Q'));
     }
 
     public function test_invalid_nif()
     {
-        $this->assertFalse($this->identity->isValidNif('21361012'));
-        $this->assertFalse($this->identity->isValidNif('1361012S'));
-        $this->assertFalse($this->identity->isValidNif('12345678F'));
-        $this->assertFalse($this->identity->isValidNif('008861617Q'));
-        $this->assertFalse($this->identity->isValidNif(''));
-        $this->assertFalse($this->identity->isValidNif(null));
+        $this->assertFalse($this->identity->isValidNIF('21361012'));
+        $this->assertFalse($this->identity->isValidNIF('1361012S'));
+        $this->assertFalse($this->identity->isValidNIF('12345678F'));
+        $this->assertFalse($this->identity->isValidNIF('008861617Q'));
+        $this->assertFalse($this->identity->isValidNIF(''));
 
-        $this->assertTrue($this->identity->isValidNif('08861617Q'));
-        $this->assertFalse($this->identity->isValidNif('08861|617Q'));
+        $this->assertTrue($this->identity->isValidNIF('08861617Q'));
+        $this->assertFalse($this->identity->isValidNIF('08861|617Q'));
 
-        $this->assertFalse($this->identity->isValidNif('X3212050P')); //nie
+        $this->assertFalse($this->identity->isValidNIF('X3212050P')); //nie
     }
 
+    public function test_throw_exception_when_nif_not_is_string_value()
+    {
+        $this->expectException(\TypeError::class);
+        $this->assertFalse($this->identity->isValidNIF(null));
+    }
 
     public function test_valid_nie()
     {
-        $this->assertTrue($this->identity->isValidNie('X3212050P'));
-        $this->assertTrue($this->identity->isValidNie('X2792997S'));
+        $this->assertTrue($this->identity->isValidNIE('X3212050P'));
+        $this->assertTrue($this->identity->isValidNIE('X2792997S'));
     }
-
 
     public function test_invalid_nie()
     {
-        $this->assertFalse($this->identity->isValidNie('Z8930474'));
-        $this->assertFalse($this->identity->isValidNie('8930474Q'));
-        $this->assertFalse($this->identity->isValidNie('Z893999Q'));
-        $this->assertFalse($this->identity->isValidNie(''));
-        $this->assertFalse($this->identity->isValidNie(null));
+        $this->assertFalse($this->identity->isValidNIE('Z8930474'));
+        $this->assertFalse($this->identity->isValidNIE('8930474Q'));
+        $this->assertFalse($this->identity->isValidNIE('Z893999Q'));
+        $this->assertFalse($this->identity->isValidNIE(''));
 
-        $this->assertTrue($this->identity->isValidNie('X3212050P'));
-        $this->assertFalse($this->identity->isValidNie('X321|2050P'));
-        $this->assertFalse($this->identity->isValidNie('08861617Q')); //nif
+        $this->assertTrue($this->identity->isValidNIE('X3212050P'));
+        $this->assertFalse($this->identity->isValidNIE('X321|2050P'));
+        $this->assertFalse($this->identity->isValidNIE('08861617Q')); //nif
     }
 
-
-    public function test_document_is_any_valid()
+    public function test_throw_exception_when_nie_not_is_string_value()
     {
-
-        $this->assertTrue($this->identity->documentIsValid('21361012S'));
-        $this->assertTrue($this->identity->documentIsValid('21361012S'));
-        $this->assertTrue($this->identity->documentIsValid('X3212050P'));
-        $this->assertFalse($this->identity->documentIsValid('Q6887124'));
-        $this->assertFalse($this->identity->documentIsValid('21361012'));
-        $this->assertFalse($this->identity->documentIsValid('Z893999Q'));
-
+        $this->expectException(\TypeError::class);
+        $this->assertFalse($this->identity->isValidNIE(null));
     }
-
 
     public function test_valid_iban()
     {
-        $this->assertTrue($this->identity->isValidIban('ES9100490013112991609374'));
-        $this->assertTrue($this->identity->isValidIban('ES09 2038-0626-0160-0002-5280'));
-        $this->assertTrue($this->identity->isValidIban('ES 58 0075 0204 9406 0081 1004'));
-        $this->assertTrue($this->identity->isValidIban('ES98 – 3190 – 0974 – 34 - 4255071823'));
-        $this->assertTrue($this->identity->isValidIban('ES31-2080-5155-9730-4000-0250'));
-        $this->assertTrue($this->identity->isValidIban('ES94 2095 5381 1910 6117 3539'));
+        $this->assertTrue($this->identity->isValidIBAN('ES9100490013112991609374'));
+        $this->assertTrue($this->identity->isValidIBAN('ES09 2038-0626-0160-0002-5280'));
+        $this->assertTrue($this->identity->isValidIBAN('ES 58 0075 0204 9406 0081 1004'));
+        $this->assertTrue($this->identity->isValidIBAN('ES98 – 3190 – 0974 – 34 - 4255071823'));
+        $this->assertTrue($this->identity->isValidIBAN('ES31-2080-5155-9730-4000-0250'));
+        $this->assertTrue($this->identity->isValidIBAN('ES94 2095 5381 1910 6117 3539'));
 
     }
-
 
     public function test_invalid_iban()
     {
 
-        $this->assertFalse($this->identity->isValidIban('ES9100490013112991609374a'));
-        $this->assertFalse($this->identity->isValidIban('9100490013112991609374a'));
-        $this->assertFalse($this->identity->isValidIban('9100490013112991609374a'));
-        $this->assertFalse($this->identity->isValidIban(''));
-        $this->assertFalse($this->identity->isValidIban(null));
+        $this->assertFalse($this->identity->isValidIBAN('ES9100490013112991609374a'));
+        $this->assertFalse($this->identity->isValidIBAN('9100490013112991609374a'));
+        $this->assertFalse($this->identity->isValidIBAN('9100490013112991609374a'));
+        $this->assertFalse($this->identity->isValidIBAN(''));
 
-        $this->assertTrue($this->identity->isValidIban('ES94 2095 5381 1910 6117 3539'));
-        $this->assertFalse($this->identity->isValidIban('ES94 2095 53|1 1910 6117 3539'));
+        $this->assertTrue($this->identity->isValidIBAN('ES94 2095 5381 1910 6117 3539'));
+        $this->assertFalse($this->identity->isValidIBAN('ES94 2095 53|1 1910 6117 3539'));
 
     }
 
+    public function test_throw_exception_when_iban_not_is_string_value()
+    {
+        $this->expectException(\TypeError::class);
+        $this->assertFalse($this->identity->isValidIBAN(null));
+    }
 }
